d92df60cd66b9c6a27cd331914f69b923cd9c921
MarcosPauloRamos <49656714+MarcosPauloRamos@users.noreply.github.com>
2021-11-03 10:43:53 -0300

    Update and rename Vida/java/Mythread.java to Vida/java/Jogo da Vida/Mythread.java

--- Vida/java/Jogo da Vida/Mythread.java
+++ Vida/java/Jogo da Vida/Mythread.java
@@ -1,15 +1,15 @@
 class Mythread implements Runnable {
 	private int TAMANHO;
 	private int MaxThreads;
-	private int Offset;
+	private int T_atual;
 	private int grid[][];
 	 int gridAux[][];
     
      /* construtor */
-	public Mythread(int tam, int maxThreads, int offset,int grafico[][]) {
+	public Mythread(int tam, int maxThreads, int t_atual,int grafico[][]) {
 		this.TAMANHO = tam;
 		this.MaxThreads = maxThreads;
-		this.Offset = offset;
+		this.T_atual = t_atual;
 		this.grid = grafico;
 		this.gridAux = new int[(tam/this.MaxThreads)][tam];
 	}
@@ -35,15 +35,15 @@ class Mythread implements Runnable {
 	public void run() {
 		for(int i=0;i<(this.TAMANHO/this.MaxThreads); i++){     
             for(int j = 0; j<this.TAMANHO; j++){
-                if (this.grid[(i+this.Offset)][j] == 1){ // //le celulas vivas
-                    if (getNeighbors((i+this.Offset),j) < 2 || getNeighbors((i+this.Offset),j) > 3){ // Regra 1
+                if (this.grid[(i+this.T_atual)][j] == 1){ // //le celulas vivas
+                    if (getNeighbors((i+this.T_atual),j) < 2 || getNeighbors((i+this.T_atual),j) > 3){ // Regra 1
                         this.gridAux[i][j] = 0;
                     }
                     else // Regra 3
                         this.gridAux[i][j] = 1; 
                 }
                 else{ // le células mortas
-                    if(getNeighbors((i+this.Offset),j) == 3) // Regra 2
+                    if(getNeighbors((i+this.T_atual),j) == 3) // Regra 2
                         this.gridAux[i][j] = 1;
                     else // Regra 3
                         this.gridAux[i][j] = 0;
